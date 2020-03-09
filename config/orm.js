e1cb63dbc53681b29e566c4c009ee619b0c1d8c6
GusGo <gus.gonzalez.15@gmail.com>
2020-03-09 17:29:42 -0400

    code cleanup

--- config/orm.js
+++ config/orm.js
@@ -6,33 +6,24 @@ const orm = {
         switch (viewType) {
             case 1:
                 let queryString1 = `SELECT * FROM employees`;
-                let query1 = connection.query(queryString1, (err, result) => {
+                connection.query(queryString1, (err, result) => {
                     if (err) throw err;
-
-                    // console.log("\n", query1.sql); // display the final query string above the results
-                    // console.log(result);
 
                     cb(result);
                 });
                 break;
             case 2:
                 let queryString2 = `SELECT name, first_name, last_name FROM employees AS e JOIN roles AS r ON e.role_id = r.id JOIN departments AS de ON r.department_id = de.id Order BY name;`
-                let query2 = connection.query(queryString2, (err, result) => {
+                connection.query(queryString2, (err, result) => {
                     if (err) throw err;
-
-                    console.log("\n", query2.sql); // display the final query string above the results
-                    // console.log(result);
 
                     cb(result);
                 });
                 break;
 
-            case 4:
-                let query4 = connection.query(`SELECT * FROM roles`, (err, result) => {
+            case 3:
+                connection.query(`SELECT * FROM roles`, (err, result) => {
                     if (err) throw err;
-
-                    console.log("\n", query4.sql); // display the final query string above the results
-                    // console.log(result);
 
                     cb(result);
                 });
@@ -47,10 +38,8 @@ const orm = {
         switch (addType) {
             case 1:
                 queryString += `(first_name, last_name, role_id, manager_id)` + extra;
-                let query1 = connection.query(queryString, ['employees', insertValues], (err, result) => {
+                connection.query(queryString, ['employees', insertValues], (err, result) => {
                     if (err) throw err;
-
-                    console.log("\n", query1.sql);
 
                     cb(result);
                 });
@@ -59,10 +48,8 @@ const orm = {
 
             case 2:
                 queryString += `(name)` + extra;
-                let query2 = connection.query(queryString, ['departments', insertValues], (err, result) => {
+                connection.query(queryString, ['departments', insertValues], (err, result) => {
                     if (err) throw err;
-
-                    console.log("\n", query2.sql);
 
                     cb(result);
                 });
@@ -71,30 +58,22 @@ const orm = {
 
             case 3:
                 queryString += `(title, salary, department_id)` + extra;
-                let query3 = connection.query(queryString, ['roles', insertValues], (err, result) => {
+                connection.query(queryString, ['roles', insertValues], (err, result) => {
                     if (err) throw err;
-
-                    console.log("\n", query3.sql);
 
                     cb(result);
                 });
         }
     },
 
-    remove: function (table, col, cb) {
-        let query = connection.query(`DELETE FROM ?? WHERE id = ?`, [table, col], (err, result) => {
+    remove: function (table, id, cb) {
+        connection.query(`DELETE FROM ?? WHERE id = ?`, [table, id], (err, result) => {
             if (err) throw err;
 
-            console.log("\n", query.sql);
-
             cb(result);
-
-        })
+        });
     }
-
-
 }
-
 
 
 module.exports = orm;
