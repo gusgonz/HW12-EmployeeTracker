eec338247db6d0f42f152a3d9304f38744d330b8
GusGo <gus.gonzalez.15@gmail.com>
2020-03-09 20:03:49 -0400

    updates for modularity and cleanliness

---
+++ models/view.js
@@ -0,0 +1,44 @@
+const orm = require("../config/orm");
+
+module.exports = (table, cb) => {
+
+    orm.view(table, result => {
+        switch (table) {
+            case 'employees':
+                var results = '\nEmployees:\n\n';
+
+                result.forEach(employee => {
+                    let niceString = `${employee.first_name} ${employee.last_name} \n`;
+                    results += niceString;
+                });
+
+                console.log(results);
+                cb();
+                break;
+
+            case 'roles':
+                var results = '\nRoles:\n\n';
+
+                result.forEach(row => {
+                    let niceString = `Title: ${row.title}, Salary: ${row.salary}, Department ID: ${row.department_id} \n`;
+                    results += niceString;
+                });
+
+                console.log(results);
+                cb();
+                break;
+
+            case null:
+                var results = '\nEmployees by Department:\n\n';
+
+                result.forEach(row => {
+                    let niceString = `${row.first_name} ${row.last_name}, ${row.name} \n`;
+                    results += niceString;
+                });
+
+                console.log(results);
+                cb();
+                break;
+        }
+    });
+}
