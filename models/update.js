5e1ba1a678792c2f797b5b182b3f14728b277229
GusGo <gus.gonzalez.15@gmail.com>
2020-03-09 20:48:52 -0400

    update model

--- models/update.js
+++ models/update.js
@@ -1,6 +1,44 @@
 const inquirer = require("inquirer");
 const orm = require("../config/orm");
 
-module.exports = (table, cb) => {
+module.exports = (route, cb) => {
 
+
+    let questions = (route === 'm') ?
+        [{
+            type: 'input',
+            message: 'Enter ID of employee whose manager you wish to update: ',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: `Enter employee's new Manager's ID: `,
+            name: 'newid'
+        }]
+        :
+        [{
+            type: 'input',
+            message: 'Enter ID of employee whose role you wish to update: ',
+            name: 'id'
+        },
+        {
+            type: 'input',
+            message: `Enter employee's new Role ID: `,
+            name: 'newid'
+        }];
+
+    inquirer
+        .prompt(questions)
+        .then(answer => {
+            let set = (route === 'r') ? { role_id: answer.newid } : { manager_id: answer.newid };
+            orm.update(set, answer.id, (result) => {
+                if (result.affectedRows === 0) {
+                    console.log('\nError updating!\n');
+                } else {
+                    console.log('\nUpdate Successful!\n');
+                }
+
+                cb();
+            });
+        });
 }
